a7557b031e8daf1ed29d8218a959f3a1babddb79
deepm24 <deep60640@gmail.com>
2023-03-12 01:03:54 +0530

    Added:edit the person contact by person name

--- AddressBook.java
+++ AddressBook.java
@@ -3,9 +3,8 @@ public class AddressBook {
     public static void main(String[] args)
     {
         System.out.println("Welcome To AddressBook System");
-        //Contact details=new Contact("deep","yadav","154","indore","mp","452010","22552465","swhdhygdje");
-
         Add addDetails=new Add();
         addDetails.addContact();
+        addDetails.editContact();
     }
 }
